ed9bf589871c113545ea7434350fc595ba738fdf
emp416 <praveen.k@finoit.co.in>
2019-09-09 18:55:57 +0530

    q

--- src/app/admin-panel/basic-ui/basic-ui.component.ts
+++ src/app/admin-panel/basic-ui/basic-ui.component.ts
@@ -7,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicUiComponent implements OnInit {
   current = 80;
-
+  progress = 0;
   constructor() { }
+  progressBar = document.querySelector('.progress-bar');
+  intervalId;
 
   ngOnInit() {
+    const getDownloadProgress = () => {
+      // console.log('getDownload', this);
+      if (this.progress <= 99) {
+        // console.log('inside if', this.progress);
+        this.progress = this.progress + 1;
+      }
+      else {
+        clearInterval(this.intervalId);
+      }
+    }
+    this.intervalId = setInterval(getDownloadProgress, 1000);
   }
     public picked(event) {
     let fileList: FileList = event.target.files;
@@ -37,6 +50,12 @@ export class BasicUiComponent implements OnInit {
     // let reader = e.target;
     // var base64result = 'data:image/png;base64, '+reader.result.substr(reader.result.indexOf(',') + 1);
     document.getElementById('image').setAttribute( 'src', e.target.result);
+  }
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+  }
+  doSomethingWithCurrentValue(event){
+    console.log(event)
   }
 
 }
