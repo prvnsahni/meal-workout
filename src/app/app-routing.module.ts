774c1dea5cad8ac0e7668a39f1b94cb815c9b5be
emp416 <praveen.k@finoit.co.in>
2019-12-10 17:49:57 +0530

    chnages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,61 +1,77 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule, ChildrenOutletContexts } from '@angular/router';
-import { AppComponent } from './app.component';
-import { MainComponent } from './user-panel/main/main.component';
-import { MealsComponent } from './user-panel/practice/meal-workout/meals/meals.component';
-import { AddMealComponent } from './user-panel/practice/meal-workout/meals/add-meal/add-meal.component';
-import { WorkoutsComponent } from './user-panel/practice/meal-workout/workouts/workouts.component';
-import { AddWorkoutComponent } from './user-panel/practice/meal-workout/workouts/add-workout/add-workout.component';
-import { LoginComponent } from './login/login.component';
-import { BasicUiComponent } from './admin-panel/basic-ui/basic-ui.component';
-import { HomeComponent } from './user-panel/home/home.component';
-import { UserPanelComponent } from './user-panel/user-panel.component';
-import { AdminPanelComponent } from './admin-panel/admin-panel.component';
-import { PracticeComponent } from './user-panel/practice/practice.component';
-import { MealWorkoutComponent } from './user-panel/practice/meal-workout/meal-workout.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule, ChildrenOutletContexts } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { MainComponent } from "./user-panel/main/main.component";
+import { MealsComponent } from "./user-panel/practice/meal-workout/meals/meals.component";
+import { AddMealComponent } from "./user-panel/practice/meal-workout/meals/add-meal/add-meal.component";
+import { WorkoutsComponent } from "./user-panel/practice/meal-workout/workouts/workouts.component";
+import { AddWorkoutComponent } from "./user-panel/practice/meal-workout/workouts/add-workout/add-workout.component";
+import { LoginComponent } from "./login/login.component";
+import { BasicUiComponent } from "./admin-panel/basic-ui/basic-ui.component";
+import { HomeComponent } from "./user-panel/home/home.component";
+import { UserPanelComponent } from "./user-panel/user-panel.component";
+import { AdminPanelComponent } from "./admin-panel/admin-panel.component";
+import { PracticeComponent } from "./user-panel/practice/practice.component";
+import { MealWorkoutComponent } from "./user-panel/practice/meal-workout/meal-workout.component";
 
 const routes: Routes = [
-  // { path: '',
-  //   component: MainComponent,
-  //   children: [
-  //     { path: 'meals',component: MealsComponent },
-  //     { path: 'workouts', component: WorkoutsComponent },
-  //   ] 
-  // },
-  // { path: 'login', component: LoginComponent },
-  // { path: 'meals/add', component: AddMealComponent },
-  // { path: 'workouts/add', component: AddWorkoutComponent },
-  { path: '',
+  {
+    path: "",
     component: AdminPanelComponent,
     children: [
-      { path: 'ui', component: BasicUiComponent},
+      {
+        path: "ui",
+        component: BasicUiComponent
+      }
     ]
   },
-  { path: '',
+  {
+    path: "",
     component: UserPanelComponent,
     children: [
-      { path: '', component: HomeComponent },
-      { path: 'home', component: HomeComponent },      
-      { path: 'portfolio', component: HomeComponent },
-      { path: 'practice', 
+      {
+        path: "",
+        component: HomeComponent
+      },
+      {
+        path: "home",
+        component: HomeComponent
+      },
+      {
+        path: "portfolio",
+        component: HomeComponent
+      },
+      {
+        path: "practice",
         component: PracticeComponent,
         children: [
-          { path: 'meal-workout', 
+          {
+            path: "meal-workout",
             component: MealWorkoutComponent,
             children: [
-              { path: 'meals', component: MealsComponent},
-              { path: 'meals/add', component: AddMealComponent },
-              { path: 'workouts', component: WorkoutsComponent},
-              { path: 'workouts/add', component: AddWorkoutComponent },
-            ] 
-          },
+              {
+                path: "meals",
+                component: MealsComponent
+              },
+              {
+                path: "meals/add",
+                component: AddMealComponent
+              },
+              {
+                path: "workouts",
+                component: WorkoutsComponent
+              },
+              {
+                path: "workouts/add",
+                component: AddWorkoutComponent
+              }
+            ]
+          }
         ]
-      },
-      
-    ] 
+      }
+    ]
   },
-  { path: 'login', component: LoginComponent },
+  { path: "login", component: LoginComponent }
 ];
 
 @NgModule({
